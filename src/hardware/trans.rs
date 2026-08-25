e462852bb8fa3a9e0b50df4b434962504a23232c
kristopherpaul <97868138+kristopherpaul@users.noreply.github.com>
2026-08-26 01:22:29 +0530

    [Trans]: Add unit tests
    Type: Test

--- src/hardware/trans.rs
+++ src/hardware/trans.rs
@@ -94,3 +94,99 @@ where
 pub enum TransError {
 
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    type TestTrans = Trans<i32>;
+
+    fn inputs(trans: TransFn<i32>, preact: i32, valid: bool, switch_transfn: bool, reset: bool) -> TransInputs<i32> {
+        TransInputs {
+            trans,
+            preact,
+            strobes: TransStrobes {
+                valid,
+                switch_transfn,
+                reset,
+            },
+        }
+    }
+
+    #[test]
+    fn new_initializes_to_relu_with_invalid_output() {
+        let trans = TestTrans::new().unwrap();
+        assert_eq!(trans.out_act(), 0);
+        assert!(!trans.out_valid());
+    }
+
+    #[test]
+    fn relu_passes_positive_values_and_clamps_negative_values() {
+        let mut trans = TestTrans::new().unwrap();
+        trans.tick(inputs(TransFn::ReLU, 7, true, false, false)).unwrap();
+        assert_eq!(trans.out_act(), 7);
+        assert!(trans.out_valid());
+
+        trans.tick(inputs(TransFn::ReLU, -3, true, false, false)).unwrap();
+        assert_eq!(trans.out_act(), 0);
+        assert!(trans.out_valid());
+    }
+
+    #[test]
+    fn leaky_relu_passes_nonnegative_values_and_scales_negative_values() {
+        let mut trans = TestTrans::new().unwrap();
+        trans.tick(inputs(TransFn::LeakyReLU(2), 5, true, true, false)).unwrap();
+        trans.tick(inputs(TransFn::LeakyReLU(2), 5, true, false, false)).unwrap();
+        assert_eq!(trans.out_act(), 5);
+        assert!(trans.out_valid());
+
+        trans.tick(inputs(TransFn::LeakyReLU(2), -4, true, false, false)).unwrap();
+        assert_eq!(trans.out_act(), -8);
+        assert!(trans.out_valid());
+    }
+
+    #[test]
+    fn switching_activation_takes_effect_on_the_following_cycle() {
+        let mut trans = TestTrans::new().unwrap();
+        trans.tick(inputs(TransFn::LeakyReLU(-2), -3, true, true, false)).unwrap();
+        assert_eq!(trans.out_act(), 0);
+        assert!(trans.out_valid());
+
+        trans.tick(inputs(TransFn::ReLU, -3, true, false, false)).unwrap();
+        assert_eq!(trans.out_act(), 6);
+        assert!(trans.out_valid());
+    }
+
+    #[test]
+    fn selected_activation_remains_active_until_switched() {
+        let mut trans = TestTrans::new().unwrap();
+        trans.tick(inputs(TransFn::LeakyReLU(-2), 0, false, true, false)).unwrap();
+        trans.tick(inputs(TransFn::ReLU, -3, true, false, false)).unwrap();
+        assert_eq!(trans.out_act(), 6);
+
+        trans.tick(inputs(TransFn::ReLU, -3, true, false, false)).unwrap();
+        assert_eq!(trans.out_act(), 6);
+    }
+
+    #[test]
+    fn invalid_input_deasserts_valid_output_without_changing_value() {
+        let mut trans = TestTrans::new().unwrap();
+        trans.tick(inputs(TransFn::ReLU, 8, true, false, false)).unwrap();
+        trans.tick(inputs(TransFn::ReLU, -99, false, false, false)).unwrap();
+
+        assert_eq!(trans.out_act(), 8);
+        assert!(!trans.out_valid());
+    }
+
+    #[test]
+    fn reset_has_priority_over_switch_and_input() {
+        let mut trans = TestTrans::new().unwrap();
+        trans.tick(inputs(TransFn::LeakyReLU(-2), -3, true, true, true)).unwrap();
+        assert_eq!(trans.out_act(), 0);
+        assert!(!trans.out_valid());
+
+        trans.tick(inputs(TransFn::ReLU, -3, true, false, false)).unwrap();
+        assert_eq!(trans.out_act(), 0);
+    }
+}
