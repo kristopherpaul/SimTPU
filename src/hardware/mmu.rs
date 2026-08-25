549a9ae60464b3067d8c12160b5ed264ce647079
kristopherpaul <97868138+kristopherpaul@users.noreply.github.com>
2026-08-25 18:23:09 +0530

    [Mmu]: Add unit tests
    Type: Test

--- src/hardware/mmu.rs
+++ src/hardware/mmu.rs
@@ -85,3 +85,68 @@ pub enum MmuError {
     #[error("pe error: {0}")]
     Pe(#[from] PeError),
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    type TestMmu = Mmu<u8, u8, u16, 2, 2>;
+
+    fn inputs(
+        weights: [u8; 2],
+        acts: [u8; 2],
+        load_weight: [bool; 2],
+        reset: bool,
+    ) -> MmuInputs<u8, u8, 2, 2> {
+        MmuInputs {
+            weights,
+            acts,
+            strobes: MmuStrobes {
+                load_weight,
+                reset,
+            },
+        }
+    }
+
+    #[test]
+    fn new_initializes_pes_and_outputs_to_default() {
+        let mmu = TestMmu::new().unwrap();
+
+        assert_eq!(mmu.pes[0][0].weight(), 0);
+        assert_eq!(mmu.pes[1][1].out_psum(), 0);
+        assert_eq!(mmu.out_acc, [0, 0]);
+    }
+
+    #[test]
+    fn staggered_activations_and_weights_produce_expected_outputs() {
+        let mut mmu = TestMmu::new().unwrap();
+
+        mmu.tick(inputs([2, 0], [0, 0], [true, false], false)).unwrap();
+        assert_eq!(mmu.pes[0][0].weight(), 2);  assert_eq!(mmu.pes[0][1].weight(), 0);
+        assert_eq!(mmu.pes[1][0].weight(), 0);  assert_eq!(mmu.pes[1][1].weight(), 0);
+        mmu.tick(inputs([0, 7], [3, 0], [false, true], false)).unwrap();
+        assert_eq!(mmu.pes[0][0].weight(), 2);  assert_eq!(mmu.pes[0][1].weight(), 7);
+        assert_eq!(mmu.pes[1][0].weight(), 0);  assert_eq!(mmu.pes[1][1].weight(), 0);
+        mmu.tick(inputs([0, 0], [5, 0], [false, false], false)).unwrap();
+        assert_eq!(mmu.pes[0][0].weight(), 2);  assert_eq!(mmu.pes[0][1].weight(), 7);
+        assert_eq!(mmu.pes[1][0].weight(), 0);  assert_eq!(mmu.pes[1][1].weight(), 0);
+        assert_eq!(mmu.out_acc, [6, 0]);
+        mmu.tick(inputs([0, 0], [0, 0], [false, false], false)).unwrap();
+        assert_eq!(mmu.out_acc, [10, 21]);
+        mmu.tick(inputs([0, 0], [0, 0], [false, false], false)).unwrap();
+        assert_eq!(mmu.out_acc, [0, 35]);
+    }
+
+    #[test]
+    fn reset_clears_pes_and_outputs() {
+        let mut mmu = TestMmu::new().unwrap();
+        mmu.tick(inputs([2, 3], [0, 0], [true, true], false)).unwrap();
+        mmu.tick(inputs([0, 0], [4, 5], [false, false], false)).unwrap();
+        mmu.tick(inputs([0, 0], [0, 0], [false, false], true)).unwrap();
+
+        assert_eq!(mmu.pes[0][0].weight(), 0);
+        assert_eq!(mmu.pes[1][1].out_psum(), 0);
+        assert_eq!(mmu.out_acc, [0, 0]);
+    }
+}
